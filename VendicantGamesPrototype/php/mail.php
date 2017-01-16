74ef792690583c789b2a1c9fb0ba7dd56a8b1c2f
npikaart <noah.pikaart.wgd@gmail.com>
2017-01-16 14:09:21 -0500

    More secure php

--- VendicantGamesPrototype/php/mail.php
+++ VendicantGamesPrototype/php/mail.php
@@ -1,12 +1,9 @@
 <?php
 $to = "noah.pikaart.wgd@gmail.com, collin.mann.wgd@gmail.com";
-$sender = $_GET["email"];
+$sender = $_POST["email"];
 $subject = "Vendicant Games Contact Form";
-$message = $_GET["message"];
+$message = $_POST["message"];
 $headers = "From: $sender";
 
 mail($to, $subject, $message, $headers);
-
-
-
 ?>
