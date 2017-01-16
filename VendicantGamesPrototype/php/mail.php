947f6c6bac7a6658c72562a8f6ed6119f20833c1
npikaart <noah.pikaart.wgd@gmail.com>
2017-01-16 13:37:01 -0500

    Fixed PHP

--- VendicantGamesPrototype/php/mail.php
+++ VendicantGamesPrototype/php/mail.php
@@ -1,12 +1,12 @@
 <?php
-$to = "noah.pikaart.wgd@gmail.com";
-$sender = $_GET("email");
+$to = "noah.pikaart.wgd@gmail.com, collin.mann.wgd@gmail.com";
+$sender = $_GET["email"];
 $subject = "Vendicant Games Contact Form";
-$message = $_GET("message") + "<br/>From:" + $_GET("fname") + $_GET("lname");
+$message = $_GET["message"];
 $headers = "From: $sender";
 
-#mail($to, $subject, $message, $headers);
+mail($to, $subject, $message, $headers);
 
-echo $_GET("email");
+
 
 ?>
