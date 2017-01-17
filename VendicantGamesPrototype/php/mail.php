24299abfb396ded8890105d3da565341befc9645
npikaart <noah.pikaart.wgd@gmail.com>
2017-01-17 13:47:36 -0500

    Updated php - remove special chars

--- VendicantGamesPrototype/php/mail.php
+++ VendicantGamesPrototype/php/mail.php
@@ -2,19 +2,26 @@
 $to = "noah.pikaart.wgd@gmail.com";
 $noreply = "noreply@kuzmaclass.org";
 $sender = $_POST["email"];
+$senderName = clean($_POST["fname"] . $_POST["lname"]);
 $subject = "Vendicant Games Contact Form";
-$message = $_POST["message"] . "\r\n \r\n" . "From: " . $sender;
+$message = clean($_POST["message"]) . "\r\n \r\n" . "From: " . $sender;
 $headers = "From: $noreply";
+$headers .= "Reply-To: $sender";
 $headers .= "MIME-Version: 1.0";
 $headers .= "Content-type:text/html; charset=UTF-8";
 
 
 
-if($message != NULL && $sender !=null) {
-    echo 'Message sent';
+if($message != NULL && $sender !=null && senderName != NULL) {
+    #echo 'Message sent';
     mail($to, $subject, $message, $headers);
 } else {
     echo 'Error sending message. Please verify that all fields were completed correctly.';
+}
+
+function clean($string) {
+    str_replace('/[^A-Za-z0-9\-]/', '', $string);
+    return $string;
 }
 
 
