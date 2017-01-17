468255eb043d8cc7cc0a07c8221d25735d6a131a
npikaart <noah.pikaart.wgd@gmail.com>
2017-01-17 14:29:02 -0500

    Updated PHP

--- VendicantGamesPrototype/php/mail.php
+++ VendicantGamesPrototype/php/mail.php
@@ -1,27 +1,17 @@
 <?php
-function clean($string) {
-    str_replace('/[^A-Za-z0-9\-]/', '', $string);
-    return $string;
-}
-
 $to = "noah.pikaart.wgd@gmail.com";
 $noreply = "noreply@kuzmaclass.org";
-$sender = $_POST["email"];
-$senderName = $_POST["fname"] . $_POST["lname"];
+$sender = preg_replace('/[^A-Za-z0-9.@\-]/', '', $_POST["email"]);
+$senderName = preg_replace('/[^A-Za-z0-9\-]/', '', $_POST["fname"] . $_POST["lname"]);
 $subject = "Vendicant Games Contact Form";
-#$message = $_POST["message"];
-$message = preg_replace('/[^A-Za-z0-9\-]/', '', $_POST["message"]);
-$message .= "\r\n \r\n" . "From: " . $sender;
+$message = preg_replace('/[^A-Za-z0-9\-]/', '', $_POST["message"]) . "\r\n \r\n" . "From: " . $sender;
 $headers = "From: $noreply";
 $headers .= "Reply-To: $sender";
 $headers .= "MIME-Version: 1.0";
 $headers .= "Content-type:text/html; charset=UTF-8";
 
-$message = preg_replace('/[^A-Za-z0-9\-]/', '', $_POST["message"]);
-$senderName = clean($senderName);
-
 if($message != NULL && $sender !=null && senderName != NULL) {
-    #echo 'Message sent';
+    echo 'Message sent';
     mail($to, $subject, $message, $headers);
 } else {
     echo 'Error sending message. Please verify that all fields were completed correctly.';
