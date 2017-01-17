2903cae96a76531e00edde61da487d31d35cb694
npikaart <noah.pikaart.wgd@gmail.com>
2017-01-17 13:56:54 -0500

    Updated php

--- VendicantGamesPrototype/php/mail.php
+++ VendicantGamesPrototype/php/mail.php
@@ -1,10 +1,16 @@
 <?php
+function clean($string) {
+    str_replace('/[^A-Za-z0-9\-]/', '', $string);
+    return $string;
+}
+
 $to = "noah.pikaart.wgd@gmail.com";
 $noreply = "noreply@kuzmaclass.org";
 $sender = $_POST["email"];
 $senderName = clean($_POST["fname"] . $_POST["lname"]);
 $subject = "Vendicant Games Contact Form";
-$message = clean($_POST["message"]) . "\r\n \r\n" . "From: " . $sender;
+$message = clean($_POST["message"]);
+$message .= "\r\n \r\n" . "From: " . $sender;
 $headers = "From: $noreply";
 $headers .= "Reply-To: $sender";
 $headers .= "MIME-Version: 1.0";
@@ -19,10 +25,7 @@ if($message != NULL && $sender !=null && senderName != NULL) {
     echo 'Error sending message. Please verify that all fields were completed correctly.';
 }
 
-function clean($string) {
-    str_replace('/[^A-Za-z0-9\-]/', '', $string);
-    return $string;
-}
+
 
 
 ?>
