1f77986225d1a18998c93592e974385f29b4d4c0
npikaart <noah.pikaart.wgd@gmail.com>
2017-01-17 13:26:09 -0500

    Updated php

--- VendicantGamesPrototype/php/mail.php
+++ VendicantGamesPrototype/php/mail.php
@@ -3,11 +3,10 @@ $to = "noah.pikaart.wgd@gmail.com";
 $noreply = "noreply@kuzmaclass.org";
 $sender = $_POST["email"];
 $subject = "Vendicant Games Contact Form";
-$message = $_POST["message"] . "<br/>" . "From: " . $sender;
+$message = $_POST["message"] . "\r\n \r\n" . "From: " . $sender;
 $headers = "From: $noreply";
 $headers .= "MIME-Version: 1.0";
-$headers .= "Content-type:text/html";
-$headers .= "charset=UTF-8";
+$headers .= "Content-type:text/html; charset=UTF-8";
 
 
 
