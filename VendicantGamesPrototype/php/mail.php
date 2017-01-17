df5de3fa2b483994413165aee9628aa76b15e5ce
npikaart <noah.pikaart.wgd@gmail.com>
2017-01-17 14:22:53 -0500

    Changed Php

--- VendicantGamesPrototype/php/mail.php
+++ VendicantGamesPrototype/php/mail.php
@@ -9,14 +9,15 @@ $noreply = "noreply@kuzmaclass.org";
 $sender = $_POST["email"];
 $senderName = $_POST["fname"] . $_POST["lname"];
 $subject = "Vendicant Games Contact Form";
-$message = $_POST["message"];
+#$message = $_POST["message"];
+$message = preg_replace('/[^A-Za-z0-9\-]/', '', $_POST["message"]);
 $message .= "\r\n \r\n" . "From: " . $sender;
 $headers = "From: $noreply";
 $headers .= "Reply-To: $sender";
 $headers .= "MIME-Version: 1.0";
 $headers .= "Content-type:text/html; charset=UTF-8";
 
-$message = clean($message);
+$message = preg_replace('/[^A-Za-z0-9\-]/', '', $_POST["message"]);
 $senderName = clean($senderName);
 
 if($message != NULL && $sender !=null && senderName != NULL) {
